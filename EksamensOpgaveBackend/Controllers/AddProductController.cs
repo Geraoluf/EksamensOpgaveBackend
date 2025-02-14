9a4c87f7613ba9e8990981a8042d449b8f7018d4
gert Rasmussen <pjente@gmail.com>
2025-02-14 14:30:31 +0100

    sidehoved

---
+++ EksamensOpgaveBackend/Controllers/AddProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EksamensOpgaveBackend.Controllers
+{
+    public class AddProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
