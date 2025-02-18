8c71319dc949bd6a0840b24a4c8fe79ca2bed83f
gert Rasmussen <pjente@gmail.com>
2025-02-18 09:42:25 +0100

    add product razorpage

--- EksamensOpgaveBackend/Controllers/AddProductController.cs
+++ EksamensOpgaveBackend/Controllers/AddProductController.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EksamensOpgaveBackend.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EksamensOpgaveBackend.Controllers
 {
     public class AddProductController : Controller
     {
+        
+        private readonly ConnectDbContext _DbContext;
+
+
+        public AddProductController(ConnectDbContext dbContext)
+        {
+            _DbContext = dbContext;
+        }
+
+
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+
+
+        public IActionResult CreateProduct()
+        {
+
+
             return View();
         }
     }
--- EksamensOpgaveBackend/Models/ProductModel.cs
+++ EksamensOpgaveBackend/Models/ProductModel.cs
@@ -1,13 +1,24 @@
-﻿namespace EksamensOpgaveBackend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EksamensOpgaveBackend.Models
 {
     public class ProductModel
     {
+            public int Id { get; set; }
 
-        public int Id { get; set; }
-        public required string Name { get; set; }
-        public string? Description { get; set; }
-        public double Price { get; set; }
-        public string? ImageUrl { get; set; }
-        public int YearOfProduction { get; set; }
+            [Required(ErrorMessage = "Du mangler at skrive et produkt navn")]
+            public string Name { get; set; }
+
+            public string? Description { get; set; }
+
+            [Required(ErrorMessage = "Pris er påkrævet")]
+            [Range(10, 150, ErrorMessage = "Pris skal være mellem 0.01 og 10.000")]
+            public double Price { get; set; }
+
+            public string? ImageUrl { get; set; }
+
+            [Range(2015, 2025, ErrorMessage = "Produktionsår skal være over 2015 og under 2025")]
+            public int YearOfProduction { get; set; }
+       
     }
 }
